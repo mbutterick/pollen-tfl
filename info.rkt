fc34339ae9495042e0de132bf5cca35786686cfd
Matthew Butterick <mb@mbtype.com>
2015-12-14 13:25:12 -0800

    adjust info.rkt

--- info.rkt
+++ info.rkt
@@ -4,4 +4,5 @@
 (define deps '("base" "pollen" "hyphenate" "css-tools" "txexpr" "sugar"))
 (define build-deps '("txexpr" "sugar" "rackunit-lib"))
 (define update-implies '("txexpr" "hyphenate" "sugar"))
-(define test-omit-paths '(all))
+(define compile-omit-paths 'all)
+(define test-omit-paths (list #rx"\\.(pm|ptree|pp)$"))
