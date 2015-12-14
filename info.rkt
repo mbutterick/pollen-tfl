00eb1b14884729a3909ba4eaa74e36f8f5205ef6
Matthew Butterick <mbutterick@users.noreply.github.com>
2015-12-13 21:49:20 -0800

    Disable build testing

--- info.rkt
+++ info.rkt
@@ -4,3 +4,4 @@
 (define deps '("base" "pollen" "hyphenate" "css-tools" "txexpr" "sugar"))
 (define build-deps '("txexpr" "sugar" "rackunit-lib"))
 (define update-implies '("txexpr" "hyphenate" "sugar"))
+(define test-omit-paths '(all))
