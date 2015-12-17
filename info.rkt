5c9071515e92fb84530c34cd478c4921620de7e9
Matthew Butterick <mb@mbtype.com>
2015-12-17 11:09:40 -0800

    adjust deps

--- info.rkt
+++ info.rkt
@@ -1,8 +1,8 @@
 #lang info
 (define collection "pollen-tfl")
 (define scribblings '(("scribblings/pollen-tfl.scrbl")))
-(define deps '("base" "pollen" "hyphenate" "css-tools" "txexpr" "sugar"))
-(define build-deps '("txexpr" "sugar" "rackunit-lib"))
+(define deps '("base" "pollen" "hyphenate" "css-tools" "txexpr" "sugar" "scribble-lib"))
+(define build-deps '("rackunit-lib" "racket-doc"))
 (define update-implies '("txexpr" "hyphenate" "sugar"))
 (define compile-omit-paths 'all)
 (define test-omit-paths (list #rx"\\.(pm|ptree|pp)$"))
