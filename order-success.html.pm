664d8299f43ed04f6df3472f9d31553cd80d2005
Matthew Butterick <mb@mbtype.com>
2017-10-30 22:00:35 -0700

    adjust order success page

--- order-success.html.pm
+++ order-success.html.pm
@@ -4,7 +4,7 @@
 ◊(section-from-metas metas)
 ◊(define-meta toolbar-blank "true")
  
-A confirmation will be sent to the email address you used for payment.
+An order confirmation and download link will be sent to the email address you used for payment.
 
 
 ◊link["/index.html"]{Return to the Typography for Lawyers home page.}
